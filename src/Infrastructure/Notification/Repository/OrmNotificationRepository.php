a9737ce3b24ecacbefa604f74dc27ffc0a7510ba
Ev.Il <xzenya@gmail.com>
2022-12-12 23:52:07 +0200

    - add OrmNotificationRepository.php

--- src/Infrastructure/Notification/Repository/OrmNotificationRepository.php
+++ src/Infrastructure/Notification/Repository/OrmNotificationRepository.php
@@ -4,9 +4,19 @@ declare(strict_types=1);
 
 namespace Infrastructure\Notification\Repository;
 
-class OrmNotificationRepository
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Domain\Notification\Notification;
+use Domain\Notification\NotificationRepositoryInterface;
+
+class OrmNotificationRepository extends ServiceEntityRepository implements NotificationRepositoryInterface
 {
-    public function __construct()
+    public function findById(int $id): ?Notification
     {
+        return $this->find($id);
+    }
+    public function add(Notification $notification): void
+    {
+        $this->getEntityManager()->persist($notification);
+        $this->getEntityManager()->flush();
     }
 }
